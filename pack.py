4028617ce3898f69869daf32a0ec5b7c0c074d14
xkball <45330674+xkball@users.noreply.github.com>
2026-01-12 19:09:32 +0800

    ci

--- pack.py
+++ pack.py
@@ -2,13 +2,9 @@ import PyInstaller.__main__
 import sys
 import os
 
-# 获取当前脚本所在目录
 current_dir = os.path.dirname(os.path.abspath(__file__))
-
-# 构建正确的文件路径
 main_script = os.path.join(current_dir, 'nrrelics', 'NRrelic_bot.py')
 
-# 构建PyInstaller命令参数
 args = [
     main_script,
     '-y',
@@ -17,10 +13,12 @@ args = [
     '--add-data=assets/:.',
 ]
 
-# 检查是否传入了短SHA
 if len(sys.argv) > 1:
     short_sha = sys.argv[1]
-    # 添加输出文件名参数，包含短SHA
-    args.append(f'--name=NRrelic_bot_{short_sha}')
+    output_file = os.environ.get("GITHUB_OUTPUT")
+    print(short_sha,output_file)
+    if output_file:
+        with open(output_file, "a") as f:
+            f.write(f"artifact_name=NRrelic_bot_NightlyBuild_{short_sha}")
 
 PyInstaller.__main__.run(args)
